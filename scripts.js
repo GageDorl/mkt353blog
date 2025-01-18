f26cf4c9f646e4ece0236994cf84e863b084a337
Gage <gagedorlando@gmail.com>
2025-01-17 22:38:44 -0700

    Site

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// let blogPost = document.getElementById('post1Content');
+// let blogInput = document.getElementById('post1Input');
+
+// blogInput.value = blogPost.innerText;
+
+// blogInput.addEventListener('input', function() {
+//     blogPost.innerText = blogInput.value;
+//     document.getElementById('wordCount').textContent = blogInput.value.split(' ').length;
+// });
+
+// textInPost = blogPost.innerText;
+// document.getElementById('wordCount').textContent = textInPost.split(' ').length;
